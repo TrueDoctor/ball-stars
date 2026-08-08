8d7f6e8c9b991757fdb71b6fdd3ece87a2922065
Dennis Kobert <dennis@kobert.dev>
2026-08-08 15:27:28 +0200

    Prototype multicast pairing

--- fetch/src/lib.rs
+++ fetch/src/lib.rs
@@ -1,14 +1,78 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{
+    io::Error,
+    marker::PhantomData,
+    net::{Ipv4Addr, SocketAddr, UdpSocket},
+};
+
+const PORT: u16 = 9999;
+const MADDR: Ipv4Addr = Ipv4Addr::new(239, 0, 0, 0);
+
+struct Pairing;
+struct Connected;
+
+struct Connection<T> {
+    peers: Vec<SocketAddr>,
+    socket: UdpSocket,
+    _state: PhantomData<T>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Connection<Pairing> {
+    fn new() -> Result<Connection<Pairing>, Error> {
+        let socket = UdpSocket::bind((MADDR, PORT))?;
+        socket.set_nonblocking(true)?;
+        socket.join_multicast_v4(&MADDR, &Ipv4Addr::UNSPECIFIED)?;
+        Ok(Self {
+            peers: vec![],
+            socket,
+            _state: Default::default(),
+        })
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn send_multicast_hello(&self) -> Result<(), Error> {
+        self.socket.send_to(&42u32.to_le_bytes(), (MADDR, PORT))?;
+
+        Ok(())
+    }
+    fn collect_responses(&mut self) -> Result<(), Error> {
+        let mut buf = [0; 4];
+        // todo deal with partial messages
+        while let Ok((read_len, addr)) = self.socket.recv_from(&mut buf) {
+            if read_len == 4 && u32::from_le_bytes(buf) == 42 && !self.peers.contains(&addr) {
+                self.peers.push(addr);
+            }
+        }
+        Ok(())
+    }
+
+    fn connect(self, addr: Ipv4Addr) -> Result<Connection<Connected>, Error> {
+        let Self { peers, .. } = self;
+
+        let socket = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, PORT))?;
+        // socket.set_nonblocking(true)?;
+        socket.connect((addr, PORT))?;
+        Ok(Connection {
+            peers,
+            socket,
+            _state: Default::default(),
+        })
+    }
+}
+
+impl Connection<Connected> {
+    fn send(&self, message: &[u8]) -> Result<(), Error> {
+        self.socket
+            .send(message)
+            .map(|len| assert_eq!(len, message.len(), "failed to send full message"))
+    }
+    fn recv(&self) -> Result<Vec<u8>, Error> {
+        let mut len = 100;
+        let mut output = Vec::new();
+        let mut buf = vec![0; 100];
+        while len == 100 {
+            len = self.socket.recv(&mut buf)?;
+            output.extend_from_slice(&buf[0..len]);
+        }
+
+        Ok(output)
     }
 }
