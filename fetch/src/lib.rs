0f4cedf00a5dbe83e7868b9b772d6644e53694ea
Dennis Kobert <dennis@kobert.dev>
2026-08-08 17:21:50 +0200

    Make network exchange more robust

--- fetch/src/lib.rs
+++ fetch/src/lib.rs
@@ -1,11 +1,15 @@
 use std::{
-    io::Error,
+    hash::{BuildHasher, Hasher, RandomState},
+    io::{Error, ErrorKind},
     marker::PhantomData,
     net::{Ipv4Addr, SocketAddr, UdpSocket},
 };
 
 const PORT: u16 = 9876;
 const MADDR: Ipv4Addr = Ipv4Addr::new(239, 0, 0, 123);
+const MAGIC: u32 = 42;
+const HELLO_LEN: usize = 12;
+const MAX_DATAGRAM: usize = 65_536;
 
 pub struct Pairing;
 pub struct Connected;
@@ -13,6 +17,7 @@ pub struct Connected;
 pub struct Connection<T> {
     peers: Vec<SocketAddr>,
     socket: UdpSocket,
+    peer_id: u64,
     _state: PhantomData<T>,
 }
 
@@ -24,24 +29,41 @@ impl Connection<Pairing> {
         Ok(Self {
             peers: vec![],
             socket,
+            peer_id: RandomState::new().build_hasher().finish(),
             _state: Default::default(),
         })
     }
 
+    pub fn peer_id(&self) -> u64 {
+        self.peer_id
+    }
+
     pub fn send_multicast_hello(&self) -> Result<(), Error> {
-        self.socket.send_to(&42u32.to_le_bytes(), (MADDR, PORT))?;
+        let mut hello = [0; HELLO_LEN];
+        hello[0..4].copy_from_slice(&MAGIC.to_le_bytes());
+        hello[4..].copy_from_slice(&self.peer_id.to_le_bytes());
+        self.socket.send_to(&hello, (MADDR, PORT))?;
 
         Ok(())
     }
+
     pub fn collect_responses(&mut self) -> Result<(), Error> {
-        let mut buf = [0; 4];
-        // todo deal with partial messages
-        while let Ok((read_len, addr)) = self.socket.recv_from(&mut buf) {
-            if read_len == 4 && u32::from_le_bytes(buf) == 42 && !self.peers.contains(&addr) {
+        let mut buf = [0; HELLO_LEN];
+        loop {
+            let (read_len, addr) = match self.socket.recv_from(&mut buf) {
+                Ok(v) => v,
+                Err(e) if e.kind() == ErrorKind::WouldBlock => return Ok(()),
+                Err(e) => return Err(e),
+            };
+            if read_len != HELLO_LEN {
+                continue;
+            }
+            let magic = u32::from_le_bytes(buf[0..4].try_into().unwrap());
+            let sender_id = u64::from_le_bytes(buf[4..].try_into().unwrap());
+            if magic == MAGIC && sender_id != self.peer_id && !self.peers.contains(&addr) {
                 self.peers.push(addr);
             }
         }
-        Ok(())
     }
 
     pub fn peers(&self) -> &[SocketAddr] {
@@ -49,7 +71,12 @@ impl Connection<Pairing> {
     }
 
     pub fn connect(self, addr: SocketAddr) -> Result<Connection<Connected>, Error> {
-        let Self { peers, socket, .. } = self;
+        let Self {
+            peers,
+            socket,
+            peer_id,
+            ..
+        } = self;
 
         socket.leave_multicast_v4(&MADDR, &Ipv4Addr::UNSPECIFIED)?;
         socket.connect(addr)?;
@@ -58,6 +85,7 @@ impl Connection<Pairing> {
         Ok(Connection {
             peers,
             socket,
+            peer_id,
             _state: Default::default(),
         })
     }
@@ -70,15 +98,15 @@ impl Connection<Connected> {
             .map(|len| assert_eq!(len, message.len(), "failed to send full message"))
     }
 
-    pub fn recv(&self) -> Result<Vec<u8>, Error> {
-        let mut len = 100;
-        let mut output = Vec::new();
-        let mut buf = vec![0; 100];
-        while len == 100 {
-            len = self.socket.recv(&mut buf)?;
-            output.extend_from_slice(&buf[0..len]);
+    pub fn recv(&self) -> Result<Option<Vec<u8>>, Error> {
+        let mut buf = vec![0; MAX_DATAGRAM];
+        match self.socket.recv(&mut buf) {
+            Ok(len) => {
+                buf.truncate(len);
+                Ok(Some(buf))
+            }
+            Err(e) if e.kind() == ErrorKind::WouldBlock => Ok(None),
+            Err(e) => Err(e),
         }
-
-        Ok(output)
     }
 }
--- fetch/src/main.rs
+++ fetch/src/main.rs
@@ -3,12 +3,15 @@ use fetch::Connection;
 fn main() -> Result<(), std::io::Error> {
     let mut connection = Connection::new()?;
 
+    println!("peer id {:x}", connection.peer_id());
+
     let peer = loop {
         connection.send_multicast_hello()?;
         std::thread::sleep(std::time::Duration::from_secs(1));
         connection.collect_responses()?;
-        if connection.peers().len() > 1 {
-            break connection.peers()[1];
+        if let Some(peer) = connection.peers().first() {
+            connection.send_multicast_hello()?;
+            break *peer;
         }
         println!("searching…");
     };
@@ -20,7 +23,9 @@ fn main() -> Result<(), std::io::Error> {
 
     loop {
         connection.send(b"hello")?;
-        let msg = connection.recv()?;
-        print!("{}", std::str::from_utf8(&msg).unwrap());
+        if let Some(msg) = connection.recv()? {
+            print!("{}", String::from_utf8_lossy(&msg));
+        }
+        std::thread::sleep(std::time::Duration::from_millis(100));
     }
 }
